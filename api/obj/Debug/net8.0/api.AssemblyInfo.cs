db460630399b512df7ad402a4063a3b836849c70
IAmTired86 <minhltt.bi12-290@st.usth.edu.vn>
2024-03-26 17:12:27 +0700

    Adding filtering using query

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+897b413fbe32936c29e35290ffffa94a99e31cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818265a4d5e221ca38a50ae906486f2bf0bb1162")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
