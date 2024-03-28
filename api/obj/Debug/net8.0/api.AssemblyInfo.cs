dc6c33b66a0ed1ab140c7326d10bef6ffe7b4263
IAmTired86 <minhltt.bi12-290@st.usth.edu.vn>
2024-03-28 09:44:06 +0700

    Identity

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b09c000f7424b2123b8a36c745376af0a91cba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5664c2d97c346e812ce49ca256c01f9b44a078")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
